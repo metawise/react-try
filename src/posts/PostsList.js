80a2d9931e1dff4b144e4a32f89c3d9e2ae0659d
Slope of Hope <buya@metawise.net>
2018-09-14 12:56:06 +0800

    git  update

---
+++ src/posts/PostsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello There</h1>
+            </div>
+        );
+    }
+}
+
+export default PostsList;
